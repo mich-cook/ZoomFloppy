fc30f2655c6277089728642eef8533623e978c9b
mich <mich-cook@users.noreply.github.com>
2022-07-03 00:05:54 -0700

    rearrange c64_sw script to group commands for packages

--- install_c64sw.sh
+++ install_c64sw.sh
@@ -8,20 +8,22 @@
 sudo apt-get -qq update && sudo apt-get install -qq git make gcc libusb-dev libncurses5-dev pkg-config
 
 # get the stuff
-mkdir /tmp/git && cd /tmp/git
-git clone https://github.com/cc65/cc65.git
-# this was the original line pointing at sf.net, but it's on
-# github too, so it's probably easier to clone from there.
-# git clone git://git.code.sf.net/p/opencbm/code opencbm
-git clone https://github.com/OpenCBM/OpenCBM.git opencbm
+mkdir /tmp/git
 
 # build cc65
-cd /tmp/git/cc65
+cd /tmp/git
+git clone https://github.com/cc65/cc65.git cc65
+cd cc65
 git checkout 93b6efcb2f969c6de0fd1eca5b07dbce18046c0a
 make && sudo PREFIX=/usr make install
 
 # build opencbm
-cd /tmp/git/opencbm
+cd /tmp/git
+# this was the original line pointing at sf.net, but it's on
+# github too, so it's probably easier to clone from there.
+# git clone git://git.code.sf.net/p/opencbm/code opencbm
+git clone https://github.com/OpenCBM/OpenCBM.git opencbm
+cd opencbm
 git checkout 0718991803fb603359b92c777d975eb06b8623b6
 make -f LINUX/Makefile opencbm plugin-xum1541
 sudo make -f LINUX/Makefile opencbm install-plugin-xum1541
